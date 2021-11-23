d3c52311c205ff524cc436b51e56d8011169e99d
unknown <sunshiny.best@gmail.com>
2021-11-23 22:05:29 +0900

    parsing_Func

--- backend.py
+++ backend.py
@@ -1,3 +1,29 @@
-# 짠
+# 장효진 , url 나누기 
+'''
+1. 길이
+2. 단축 URL
+3. @ 포함
+4. // 포함
+5. - 포함
+6. 서브도메인 갯수
+7. http/https 사용여부
+'''
+#parsing
+# pip install parse
+from parse import *
 
-# 긴 url
+def parsing(input_url):
+    # local_input = input_url
+    # protocal = local_input.split("//")
+    # print(protocal)
+    
+    result = parse("{protocol}//{domain}/{path}", input_url)
+    print(result["protocol"])
+
+parsing("https://urp_success/123")
+
+# 길이 
+
+# 단축 url
+
+# @포함 
